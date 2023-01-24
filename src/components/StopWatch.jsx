ae840711f58c2cdedfc989250b470d876538d5bf
Evgeniy Reznichenko <ereznichenko@gmail.com>
2023-01-25 01:58:15 +0200

    Temp StopWatch implementation

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -1,11 +1,52 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import {Box, Button, Typography, useTheme} from "@mui/material";
 import { tokens } from "../theme";
 import WatchLaterOutlined from "@mui/icons-material/WatchLaterOutlined";
 
+const WatchStatus = {
+    running: 'running',
+    stopped: 'stopped'
+}
+function Watch(){
+    let start;
+    let stop;
+    let status;
+
+    Object.defineProperty(this, 'duration', {
+        get: function(){
+            if(start && status)
+                return start - new Date();
+        }
+    });
+
+    this.start = function(){
+        if(status === WatchStatus.running){
+
+        }
+        start = new Date();
+        status = WatchStatus.running;
+    };
+
+    this.stop = function(){
+        stop = new Date();
+        status = WatchStatus.stopped;
+    };
+
+    this.resume = function(){
+        this.start();
+        stop = null;
+    };
+
+    this.reset = function(){
+        start = null;
+        stop = null;
+        status = null;
+    };
+}
 
 const StopWatch = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
+    const watch = new Watch();
 
     return (
         <Box width="100%" m="0 30px">
@@ -21,7 +62,17 @@ const StopWatch = () => {
                     </Typography>
                 </Box>
                 <Box>
-                    {/*<ProgressCircle progress={progress} />*/}
+                    <Button
+                        sx={{
+                            backgroundColor: colors.blueAccent[700],
+                            color: colors.grey[100],
+                            fontSize: "14px",
+                            fontWeight: "bold",
+                            padding: "10px 20px",
+                        }}
+                    >
+                        Start
+                    </Button>
                 </Box>
             </Box>
             <Box display="flex" justifyContent="space-between" mt="2px">
